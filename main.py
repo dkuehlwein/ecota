49c87ed3118f1efce89caf3428c5d7ece04c7890
Daniel Kühlwein <daniel.kuehlwein@capgemini.com>
2022-07-29 18:30:42 +0200

    Added 5-min capture interval

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from picamera import PiCamera
+from datetime import datetime
 from time import sleep
 
 camera = PiCamera()
 camera.resolution = (2592, 1944)
 
-camera.start_preview()
-sleep(5)
-camera.capture('/home/pi/pytest.jpg')
-camera.stop_preview()
+while True:
+    camera.start_preview()
+    sleep(5)
+    now = datetime.now()
+    file_name = f'/home/pi/data/{now}.jpg'
+    camera.capture(file_name)
+    camera.stop_preview()
+    sleep(295)
