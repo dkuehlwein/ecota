65f9a0dff49517fde601982d297f0c957e74691a
Daniel Kühlwein <daniel.kuehlwein@capgemini.com>
2022-07-29 18:34:44 +0200

    need a string

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ while True:
     camera.start_preview()
     sleep(5)
     now = datetime.now().strftime("%Y-%m-%d--%H:%M:%S")
-    file_name = f'/home/pi/data/{now}.jpg'
+    file_name = f'data/{now}.jpg'
     print(f'Saving img at {file_name}')
     camera.capture(file_name)
     camera.stop_preview()
